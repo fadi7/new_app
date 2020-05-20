a680c0eae7434e216bdf9af8d16140ab75c1c963
fadi7 <mfk17@live.com>
2020-05-20 07:18:37 +0400

    before list title

--- lib/screens/home_tabs/tab_three.dart
+++ lib/screens/home_tabs/tab_three.dart
@@ -3,6 +3,31 @@ import 'package:flutter/material.dart';
 class TabThree extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return Card(
+          color: Color(0XFF1a2f41),
+          child: Container(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                _drawAuthorRow(),
+              ],
+            ),
+          ),
+        );
+      },
+      itemCount: 10,
+    );
+  }
+
+  Widget _drawAuthorRow() {
+    return Row(
+      children: <Widget>[
+        CircleAvatar(
+          backgroundColor: Colors.amber,
+        ),
+      ],
+    );
   }
 }
